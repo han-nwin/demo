e05895eee885a3f2088faae7cf523a16e4ed3d40
han-nwin <hannguyen.win@gmail.com>
2025-02-06 14:52:12 -0600

    logo spin

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import reactLogo from "./assets/react.svg"
-import viteLogo from "/vite.svg"
+// import viteLogo from "/vite.svg"
 import "./App.css"
 
 // Props Example
@@ -117,7 +117,7 @@ function App() {
           {/*   <img src={viteLogo} className="logo" alt="Vite logo" /> */}
           {/* </a> */}
           <a href="https://react.dev" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
+            <img src={reactLogo} className="logo" alt="React logo" />
           </a>
         </div>
         <h1>React Features Demo</h1>
